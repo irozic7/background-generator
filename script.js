6791abb3500c7127fce734f4a1af96314a2162a3
Ivan Rožić <44834260+irozic7@users.noreply.github.com>
2020-09-30 15:01:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background =
+		"linear-gradient(to right, " 
+		+ color1.value 
+		+ ", " 
+		+ color2.value 
+		+ ")";
+
+		css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+//ILI U HTML ..> oninput="setGradient()"
+
+color2.addEventListener("input", setGradient);
+
+
+
+//*************** SKRATILI SMO OVAJ KOD S OVIM GORE ****************//
+
+// color1.addEventListener("input", function(){
+// 	body.style.background =
+// 		"linear-gradient(to right, " 
+// 		+ color1.value 
+// 		+ ", " 
+// 		+ color2.value 
+// 		+ ")";
+// })
+
+// color2.addEventListener("input", function(){
+// 		body.style.background =
+// 		"linear-gradient(to right, " 
+// 		+ color1.value 
+// 		+ ", " 
+// 		+ color2.value 
+// 		+ ")";
+// })
+
